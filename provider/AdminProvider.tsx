148a367269b8e123108e11cfa4eb5d2110ffa0bf
ABUBAKARKHAN-Stack <abubakar123456654123@gmail.com>
2026-01-27 16:39:13 +0500

    feat: Add AdminProvider component to consolidate global contexts and define distinct cursor styles for admin and public interfaces.

--- provider/AdminProvider.tsx
+++ provider/AdminProvider.tsx
@@ -6,7 +6,7 @@ import { queryClient } from '@/lib/query-client'
 import { QueryClientProvider } from '@tanstack/react-query'
 import  { ReactNode } from 'react'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
+import "./admin.css"
 
 type Props = {
     children: ReactNode
